26cec12f57ca312f0a8b39b393ef5385f132330b
Jeanne Mas <jeannemas@jeannemas.me>
2024-06-30 11:48:01 -0400

    Removed internal reference to self in config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
 // @ts-check
 
-import documentizePreprocessor from '@jeanne-mas/documentize-preprocessor';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { resolve } from 'node:path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [documentizePreprocessor(resolve('./tsconfig.json')), vitePreprocess()],
+  preprocess: [vitePreprocess()],
 
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
